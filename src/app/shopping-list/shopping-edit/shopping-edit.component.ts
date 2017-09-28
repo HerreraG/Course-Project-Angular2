1072e9847634bb5fa93e35af0e831af025989b30
Gonzalo <hgonzalo94@gmail.com>
2017-09-28 00:41:17 -0300

    Ability of add an item

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, ElementRef, ViewChild} from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput') nameInputReference: ElementRef;
+  @ViewChild('numberInput') numberInputReference: ElementRef;
+  ingredientAdded = new EventEmitter<Ingredient>();
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onAddItem() {
+    const ingName = this.nameInputReference.nativeElement.value;
+    const ingAmount = this.numberInputReference.nativeElement.value;
+    const newIngredient = new Ingredient(ingName, ingAmount);
+
+    this.ingredientAdded.emit(newIngredient);
+  }
 }
